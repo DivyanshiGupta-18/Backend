28b1175000a3262b8901b2bea8194ecbd976c67b
DivyanshiGupta-18 <guptadivyanshi756@gmail.com>
2025-09-08 12:44:51 +0530

    vd-87 completed

---
+++ Day 2/aboutpath.js
@@ -0,0 +1,11 @@
+import path from "path"
+
+let mypath = "c:\\Users\\lenovo\\OneDrive\\Desktop\\Web D\\Backend\\Day 2\\drift.txt"
+console.log(path.extname(mypath));
+
+
+console.log(path.dirname(mypath));
+console.log(path.basename(mypath));
+
+
+console.log(path.join("c:/", "programs\\drift.txt" ))
--- Day 2/mainpromise.js
+++ Day 2/mainpromise.js
@@ -2,6 +2,6 @@ import fs from "fs/promises"
 
 let a = await fs.readFile("drift.txt")
 
-let b = await fs.writeFile("drift.txt",
-    "\n\n\n\nthis is amazing promise")
+let b = await fs.appendFile("drift.txt", 
+    "\n\n\n\n this is amazing promise")
 console.log(a.toString(), b)
