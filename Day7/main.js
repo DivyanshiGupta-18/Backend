da9a2eda3a68045c3f92c7e8ae0c7526890492f7
DivyanshiGupta-18 <guptadivyanshi756@gmail.com>
2025-10-06 17:54:24 +0530

    problem statement of day7 vd-93

--- Day7/main.js
+++ Day7/main.js
@@ -1,5 +1,23 @@
 // sab files ko unke folder ke acc. save karna hai
 
+// You have to write a Node.js program to clear clutter inside of a directory and organize the contents of that directory into different folders
+
+// for example, these files become:
+
+// 1. name.jpg
+// 2. name.png
+// 3. this.pdf 
+// 4. harry.zip
+// 5. Rohan.zip
+// 6. cat.jpg 
+// 7. harry.pdf
+
+// this: 
+// jpg/name.jpg, jpg/cat.jpg 
+// png/name.png 
+// pdf/this.pdf pdf/harry.pdf
+// zip/harry.zip zip/Rohan.zip
+
 import fs from "fs/promises";
 import fsn from "fs";
 import path from "path";
